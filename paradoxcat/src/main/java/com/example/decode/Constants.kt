ac049fb5c9de67d096113be3f972b0ff52523ef2
Vahid Moradi <zvm4zvm@gmail.com>
2022-05-22 15:33:03 +0430

    final tidy up

---
+++ paradoxcat/src/main/java/com/example/decode/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.decode
+
+val LEADER_VALUE = "ff".toInt(16)
+val DIVIDER_VALUE = "0".toInt(16)
+val ID_FIRST_VALUE = "42".toInt(16)
+val ID_SECOND_VALUE = "3".toInt(16)
+const val ID_BYTE_SIZE = 1
+const val DIVIDER_SIZE = 1
+const val LEADER_BYTE_SIZE = 652
+const val MESSAGE_BYTE_SIZE = 1984
+const val END_BLOCK_BYTE_SIZE = 130 - 1
--- paradoxcat/src/main/java/com/example/decode/SecretDetector.kt
+++ paradoxcat/src/main/java/com/example/decode/SecretDetector.kt
@@ -6,17 +6,6 @@ import com.example.wavedecoder.library.*
 import kotlinx.coroutines.flow.*
 import java.io.File
 
-val LEADER_VALUE = "ff".toInt(16)
-val DIVIDER_VALUE = "0".toInt(16)
-val ID_FIRST_VALUE = "42".toInt(16)
-val ID_SECOND_VALUE = "3".toInt(16)
-
-const val ID_BYTE_SIZE = 1
-const val DIVIDER_SIZE = 1
-const val LEADER_BYTE_SIZE = 652
-const val MESSAGE_BYTE_SIZE = 1984
-const val END_BLOCK_BYTE_SIZE = 130 - 1
-
 /**
  * https://medium.com/poka-techblog/back-to-basics-decoding-audio-modems-with-audacity-c94faa8362a0
  */
