5bc80583b2b227f4eb6301765ac3a4a21d72c88c
Vahid Moradi <zvm4zvm@gmail.com>
2022-05-22 10:23:49 +0430

    add FlowSequenceFinderTest tests

--- decode/src/test/java/com/example/decode/FlowSequenceFinderTest.kt
+++ decode/src/test/java/com/example/decode/FlowSequenceFinderTest.kt
@@ -11,30 +11,85 @@ import org.junit.Test
 
 class FlowSequenceFinderTest {
     @Test
-    fun AAAAAAAAAAAAA() = runBlocking {
-        println("test")
-        Assert.assertEquals(2, 2)
+    fun skipOccurrenceSequence_onStopFind_stopsOnFind() = runBlocking {
         val result = flowOf(1, 1, 1, 1, 2)
-            .skipOccurrenceSequence(1, 2)
             .skipOccurrenceSequence(1, 2, true)
             .toList()
-        println(result)
-        Assert.assertEquals(2, 2)
-
+        Assert.assertEquals(0, result.size)
     }
 
     @Test
-    fun BBBB() = runBlocking {
-        println("asdasd")
-        val result = flowOf(0, 1, 2, 3, 4)
-            .skipOccurrenceSequenceWithData(4, callback = {
-                println(it)
-            })
+    fun skipOccurrenceSequence_skipFirst_shouldSkip() = runBlocking {
+        val result = flowOf(1, 1, 1, 1, 2)
+            .skipOccurrenceSequence(1, 2)
             .toList()
-        println(result)
-        Assert.assertEquals(2, 2)
-
+        Assert.assertEquals(listOf(1, 1, 2), result)
     }
+
+    @Test
+    fun skipOccurrenceSequence_middleRange_shouldReturnZero() = runBlocking {
+        val result = flowOf(4, 5, 1, 1, 2)
+            .skipOccurrenceSequence(1, 2)
+            .toList()
+        Assert.assertEquals(0, result.size)
+    }
+
+    @Test
+    fun skipOccurrenceSequence_withOneItem_shouldSkip() = runBlocking {
+        val result = flowOf(1)
+            .skipOccurrenceSequence(1, 1)
+            .toList()
+        Assert.assertEquals(0, result.size)
+    }
+
+    @Test
+    fun skipOccurrenceSequence_withTwoItemFirstOccurrence_shouldReturnsSecond() = runBlocking {
+        val result = flowOf(1, 2)
+            .skipOccurrenceSequence(1, 1)
+            .toList()
+        Assert.assertEquals(1, result.size)
+        Assert.assertEquals(2, result.first())
+    }
+
+    @Test
+    fun skipOccurrenceSequence_nestedSkip_shouldWorks() = runBlocking {
+        val result = flowOf(1, 1, 1, 2)
+            .skipOccurrenceSequence(1, 1)
+            .skipOccurrenceSequence(1, 1)
+            .skipOccurrenceSequence(1, 1)
+            .toList()
+        Assert.assertEquals(1, result.size)
+        Assert.assertEquals(2, result.first())
+    }
+
+    @Test
+    fun skipOccurrenceSequenceWithData_nestedSkipWithSameItems_shouldWorks() = runBlocking {
+        var callbackData: List<Int>? = null
+        val callback = { data: List<Int> ->
+            callbackData = data
+        }
+        val result = flowOf(1, 1, 1, 1)
+            .skipOccurrenceSequenceWithData(1, callback)
+            .toList()
+
+        Assert.assertEquals(listOf(1, 1, 1), result)
+        Assert.assertEquals(listOf(1), callbackData)
+    }
+
+    @Test
+    fun skipOccurrenceSequenceWithData_nestedSkipWithDifferentItems_shouldWorks() = runBlocking {
+        var callbackData: List<Int>? = null
+        val callback = { data: List<Int> ->
+            callbackData = data
+        }
+        val result = flowOf(1, 2, 3, 4)
+            .skipOccurrenceSequenceWithData(1, callback)
+            .toList()
+
+        Assert.assertEquals(listOf(2, 3, 4), result)
+        Assert.assertEquals(listOf(1), callbackData)
+    }
+
 
 }
 
